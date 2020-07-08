e0b194344077872ad0327f111c5c6c4c7ec3ac95
ShaikAbdulRazzaaq <46188045+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-07-08 20:46:26 +0530

    Something is Changed Need to check

--- app/src/main/java/com/projects/ebookapp/MainActivity.java
+++ app/src/main/java/com/projects/ebookapp/MainActivity.java
@@ -2,14 +2,12 @@ package com.projects.ebookapp;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     private static final String TAG = "MainActivity";
 
@@ -18,25 +16,38 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavigationBar);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.categories: {
-                Log.d(TAG, "onCreate: Categories");
-                break;
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.categories: {
+                    Log.d(TAG, "onCreate: Categories");
+                    break;
+                }
+                case R.id.favorites: {
+                    Log.d(TAG, "onCreate: Favorites");
+                    break;
+                }
+                case R.id.home: {
+                    Log.d(TAG, "onCreate: Home");
+                    break;
+                }
             }
-            case R.id.favorites: {
-                Log.d(TAG, "onCreate: Favorites");
-                break;
+            return false;
+        });
+        bottomNavigationView.setOnNavigationItemReselectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.categories: {
+                    Log.d(TAG, "onCreate: On Reselect Categories");
+                    break;
+                }
+                case R.id.favorites: {
+                    Log.d(TAG, "onCreate: On Reselect Favorites");
+                    break;
+                }
+                case R.id.home: {
+                    Log.d(TAG, "onCreate: On Reselect Home");
+                    break;
+                }
             }
-            case R.id.home: {
-                Log.d(TAG, "onCreate: Home");
-                break;
-            }
-        }
-        return false;
+        });
     }
 }
