a23338be1033a700e969d4a06776b0dbabf609a6
ShaikAbdulRazzaaq <46188045+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-07-08 20:22:47 +0530

    Started Working On BottomNavigationLayout Code

--- app/src/main/java/com/projects/ebookapp/MainActivity.java
+++ app/src/main/java/com/projects/ebookapp/MainActivity.java
@@ -1,14 +1,42 @@
 package com.projects.ebookapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationBar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.categories: {
+                Log.d(TAG, "onCreate: Categories");
+                break;
+            }
+            case R.id.favorites: {
+                Log.d(TAG, "onCreate: Favorites");
+                break;
+            }
+            case R.id.home: {
+                Log.d(TAG, "onCreate: Home");
+                break;
+            }
+        }
+        return false;
     }
 }
