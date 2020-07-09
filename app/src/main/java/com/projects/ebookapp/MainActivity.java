f7cb75a35f2b8b7ce3f205c08cbbe4498d0c97b6
ShaikAbdulRazzaaq <46188045+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-07-09 11:58:08 +0530

    Fragment Switching SuccessFul

--- app/src/main/java/com/projects/ebookapp/MainActivity.java
+++ app/src/main/java/com/projects/ebookapp/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new CategoriesFragment());
         bottomNavigationView = findViewById(R.id.bottomNavigationBar);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             Fragment fragment = null;
