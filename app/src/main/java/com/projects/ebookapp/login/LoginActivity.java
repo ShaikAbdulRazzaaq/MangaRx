4d93dc62554a1353d892780d5beb7728f6f5e9d5
ShaikAbdulRazzaaq <46188045+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-07-09 20:55:12 +0530

    Further Improved Authorization Code

--- app/src/main/java/com/projects/ebookapp/login/LoginActivity.java
+++ app/src/main/java/com/projects/ebookapp/login/LoginActivity.java
@@ -4,16 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.projects.ebookapp.MainActivity;
 import com.projects.ebookapp.R;
+
+import java.util.Objects;
+
+import static com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG;
 
 public class LoginActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
@@ -22,6 +30,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText loginPassword;
     Button login;
     Button signup;
+    CoordinatorLayout coordinatorLayout;
+    ProgressBar progressBar;
     private static final String TAG = "LoginActivity";
 
     @Override
@@ -33,7 +43,12 @@ public class LoginActivity extends AppCompatActivity {
         name = findViewById(R.id.et_yourName);
         login = findViewById(R.id.btn_login);
         signup = findViewById(R.id.btn_signUp);
-        login.setOnClickListener(v -> validateCredentials(name.getText().toString(), loginPassword.getText().toString()));
+        progressBar = findViewById(R.id.loginProgress);
+        coordinatorLayout = findViewById(R.id.coordinatorLoginPage);
+        login.setOnClickListener(v -> {
+            progressBar.setVisibility(View.VISIBLE);
+            validateCredentials(name.getText().toString(), loginPassword.getText().toString());
+        });
         signup.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, SignUpActivity.class)));
     }
 
@@ -46,15 +61,18 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             firebaseAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
+                    progressBar.setVisibility(View.INVISIBLE);
                     Toast.makeText(this, "Login SuccessFull", Toast.LENGTH_SHORT).show();
                     firebaseUser = firebaseAuth.getCurrentUser();
                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
                 } else {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Snackbar.make(coordinatorLayout, Objects.requireNonNull(Objects.requireNonNull(task.getException()).getMessage()), LENGTH_LONG).show();
                     Log.e(TAG, "validateCredentials: ", task.getException());
                 }
             });
         }
     }
-
 
 }
--- app/src/main/java/com/projects/ebookapp/login/SignUpActivity.java
+++ app/src/main/java/com/projects/ebookapp/login/SignUpActivity.java
@@ -4,14 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.projects.ebookapp.R;
+
+import java.util.Objects;
+
+import static com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG;
 
 public class SignUpActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
@@ -19,6 +27,8 @@ public class SignUpActivity extends AppCompatActivity {
     EditText password;
     Button signUp;
     private static final String TAG = "SignUpActivity";
+    CoordinatorLayout coordinatorLayout;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,13 @@ public class SignUpActivity extends AppCompatActivity {
         username = findViewById(R.id.et_howCanICallYou);
         password = findViewById(R.id.et_password);
         signUp = findViewById(R.id.btn_signUpToLogin);
-        signUp.setOnClickListener(v -> validateCredentials(username.getText().toString(), password.getText().toString()));
+        coordinatorLayout = findViewById(R.id.coordinator);
+        progressBar = findViewById(R.id.signUpProgress);
+        signUp.setOnClickListener(v -> {
+            progressBar.setVisibility(View.VISIBLE);
+            validateCredentials(username.getText().toString(), password.getText().toString());
+        });
+
     }
 
     private void validateCredentials(String userName, String passWord) {
@@ -38,9 +54,13 @@ public class SignUpActivity extends AppCompatActivity {
         } else {
             firebaseAuth.createUserWithEmailAndPassword(userName, passWord).addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
+                    progressBar.setVisibility(View.INVISIBLE);
                     Toast.makeText(this, "Signed Up successfully", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                    finish();
                 } else {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Snackbar.make(coordinatorLayout, Objects.requireNonNull(Objects.requireNonNull(task.getException()).getMessage()), LENGTH_LONG).show();
                     Log.e(TAG, "validateCredentials: ", task.getException());
                 }
             });
