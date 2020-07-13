d0266004a5905b5c3ac458e2bc1c805897326987
ShaikAbdulRazzaaq <46188045+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-07-13 12:09:24 +0530

    Further Improved Authorization Code

--- app/src/main/java/com/projects/ebookapp/fragments/CategoriesFragment.java
+++ app/src/main/java/com/projects/ebookapp/fragments/CategoriesFragment.java
@@ -15,6 +15,6 @@ public class CategoriesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.categories_fragment,container,false);
+        return inflater.inflate(R.layout.categories_fragment, container, false);
     }
 }
