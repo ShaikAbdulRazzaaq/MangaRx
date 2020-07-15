1e94e50c0c5a90e90aab6df7340ae5368a4cfd39
ShaikAbdulRazzaaq <46188045+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-07-15 16:37:12 +0530

    On Facebook it is saying key hashes don't match

--- app/src/main/java/com/projects/ebookapp/login/LoginActivity.kt
+++ app/src/main/java/com/projects/ebookapp/login/LoginActivity.kt
@@ -11,26 +11,36 @@ import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.facebook.*
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.projects.ebookapp.MainActivity
 import com.projects.ebookapp.R
 
 class LoginActivity : AppCompatActivity() {
+
     private var firebaseAuth: FirebaseAuth? = null
     private var firebaseUser: FirebaseUser? = null
-    var name: EditText? = null
+    private var name: EditText? = null
     private var loginPassword: EditText? = null
     private var login: Button? = null
     private var signup: Button? = null
     private var coordinatorLayout: CoordinatorLayout? = null
     private var progressBar: ProgressBar? = null
+    private var facebookLogin: LoginButton? = null
+    private val callbackManager = CallbackManager.Factory.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppEventsLogger.activateApp(application)
         setContentView(R.layout.activity_login)
         firebaseAuth = FirebaseAuth.getInstance()
         loginPassword = findViewById(R.id.loginPassword)
@@ -39,11 +49,42 @@ class LoginActivity : AppCompatActivity() {
         signup = findViewById(R.id.btn_signUp)
         progressBar = findViewById(R.id.loginProgress)
         coordinatorLayout = findViewById(R.id.coordinatorLoginPage)
+        facebookLogin = findViewById(R.id.facebookLoginButton)
         login?.setOnClickListener {
             progressBar?.visibility = View.VISIBLE
             validateCredentials(name?.text.toString(), loginPassword?.text.toString())
         }
+
         signup?.setOnClickListener { startActivity(Intent(this@LoginActivity, SignUpActivity::class.java)) }
+        facebookLogin?.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                Log.d(TAG, "The login Result is $result  ")
+                accessToken(result?.accessToken)
+            }
+
+            override fun onCancel() {
+                Snackbar.make(coordinatorLayout!!, "Cancelled", BaseTransientBottomBar.LENGTH_LONG).show()
+            }
+
+            override fun onError(error: FacebookException?) {
+                Log.e(TAG, ": $error")
+            }
+
+        })
+    }
+
+    private fun accessToken(accessToken: AccessToken?) {
+        val authCredential = FacebookAuthProvider.getCredential(accessToken?.token.toString())
+        firebaseAuth?.signInWithCredential(authCredential)?.addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                progressBar?.visibility = View.VISIBLE
+                Log.d(TAG, "signInWithCredential:success")
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                Snackbar.make(coordinatorLayout!!, "" + task.exception?.message, BaseTransientBottomBar.LENGTH_LONG).show()
+            }
+        }
     }
 
     private fun validateCredentials(username: String, password: String) {
@@ -65,6 +106,11 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     companion object {
